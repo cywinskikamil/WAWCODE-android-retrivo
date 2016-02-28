744160508747fc1d0888b59e8adac6c3dd63f5a8
Kamil Cywiński <cywinski.kamil@yahoo.com>
2016-02-28 09:52:54 +0100

    zamiana obrazka do stringu

--- Retrovi/app/src/main/java/banany/retrovi/JSONCollection.java
+++ Retrovi/app/src/main/java/banany/retrovi/JSONCollection.java
@@ -2,13 +2,16 @@ package banany.retrovi;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.util.Log;
+import android.graphics.Bitmap;
+import android.util.Base64;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
@@ -22,17 +25,35 @@ import java.util.Map;
 public class JSONCollection {
     private static TextView tv;
 
-    private static Type type = new TypeToken<Map<String, String>>(){}.getType();
+    private static Type type = new TypeToken<Map<String, String>>() {
+    }.getType();
 
-    public static Map <String, String> mapFromJSON (Resources resources) {
+    public static Map<String, String> mapFromJSON(Resources resources) {
         Gson gson = new Gson();
         InputStream inputStream = resources.openRawResource(R.raw.przykladowy);
         return gson.fromJson(new InputStreamReader(inputStream), type);
     }
 
-    public static void JSONFromMap(Context context, Map <String, String> stringStringMap){
+    public static String JSONFromMap(Context context, Map<String, String> stringStringMap) {
         Gson gson = new GsonBuilder().create();
-        String json = gson.toJson(stringStringMap, type);
-        Log.d("a", json);
+        return gson.toJson(stringStringMap, type);
+    }
+
+    public static String ImageViewToString(ImageView imageView) {
+        imageView.setDrawingCacheEnabled(true);
+        imageView.measure(ImageView.MeasureSpec.makeMeasureSpec(0, ImageView.MeasureSpec.UNSPECIFIED),
+                ImageView.MeasureSpec.makeMeasureSpec(0, ImageView.MeasureSpec.UNSPECIFIED));
+        imageView.layout(0, 0, imageView.getMeasuredWidth(), imageView.getMeasuredHeight());
+        imageView.buildDrawingCache();
+
+        Bitmap bitmap = Bitmap.createBitmap(imageView.getDrawingCache());
+        imageView.setDrawingCacheEnabled(false); //
+
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] b = baos.toByteArray();
+
+        return Base64.encodeToString(b, Base64.DEFAULT);
     }
 }
